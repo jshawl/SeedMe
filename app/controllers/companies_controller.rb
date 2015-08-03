226aa2f70a461646a09efcba4cdab1eaab69bf1f
Jeff Jones <jeff.jones1@gmail.com>
2015-08-03 14:12:31 -0400

    created new company form, added new/create to controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,25 @@
 class CompaniesController < ApplicationController
+  before_action :get_company, only: [:show, :edit, :update, :destroy]
+
   def index
-    @companies = Company.all
+    @companies = Company.all.order(created_at: :desc)
   end
 
   def show
-    @company = Company.find(params[:id])
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+  end
+
+  private
+    def company_params
+        params.require(:company).permit(:name, :industry, :pitch_text, :pitch_url)
+    end
+    def get_company
+      @company = Company.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   end
 
   resources :companies, only: [:index]
+  
+  get '/sign_in', to: 'users#sign_in'
+  post '/sign_in', to: 'users#sign_in!'
+  get '/sign_up', to: 'users#sign_up'
+  post '/sign_up', to: 'users#sign_up!'
+  get '/sign_out', to: 'users#sign_out'
+
 end
