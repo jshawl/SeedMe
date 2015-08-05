a05e9259c7492e006db92753973d94d84bd09f93
Jeff Jones <jeff.jones1@gmail.com>
2015-08-05 12:18:37 -0400

    tweaked more css

--- app/controllers/favorite_companies_controller.rb
+++ app/controllers/favorite_companies_controller.rb
@@ -13,7 +13,7 @@ class FavoriteCompaniesController < ApplicationController
     Favorite.where(favorited_id: @company.id, user_id: current_user.id).first.destroy
     redirect_to  :back, notice: 'Company is no longer in favorites'
   end
-  
+
   def index
     @favorites = current_user.favorite_companies
   end
