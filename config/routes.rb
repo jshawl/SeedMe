e88ca3aa3c3f915c8938b96b56ed85c7069d24f5
Jeff Jones <jeff.jones1@gmail.com>
2015-08-05 10:40:19 -0400

    renested upvoting under companies for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root to: "companies#index"
   resources :users do
-    resources :companies do
-        put 'upvote', to: "companies#upvote"
-    end
+    resources :companies
   end
   resources :sessions
   resources :companies, only: [:index, :show, :create] do
     resources :favorite_companies, only: [:create, :destroy]
+    put 'upvote', to: "companies#upvote"
   end
 
   get 'signup', to: 'users#new', as: 'signup'
